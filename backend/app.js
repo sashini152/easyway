d06c0f231af4d45de9b59c11070ef53808d4afea
senrithfernando <rihansenrithvta@gmail.com>
2026-03-21 09:52:49 +0530

    fixed(backend):db connection error

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,18 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
 
 import userRoutes from "./routes/UserRoute.js";
-
-dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// DB connect
-mongoose.connect(process.env.MONGO_STRING)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
 // Routes
 app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 export default app;
